70622ab8a55eefdfebc7ee3e42e29316dc059fb2
rferrazd <56007093+rferrazd@users.noreply.github.com>
2023-02-02 15:52:54 -0800

    alternative solutions for homework 2

--- homework/homework2/HighArray.py
+++ homework/homework2/HighArray.py
@@ -1,5 +1,5 @@
 ###
-# filename: HighArray.py
+# filename: HighArray.py 
 # purpose:  @see homework02 description at:
 #    https://bjohnson.lmu.build/cmsi3530web/homework02.html
 # author:   Dr. Johnson
@@ -8,12 +8,12 @@
 
 # global to the entire file
 a = []         # empty list
-max = 0
+
 # the class
 class HighArray():
 
    # fields
-      
+
    # initializer
    def __init__( self, maxSize ):
       a = []         # initialize the list to be empty [still]
@@ -36,25 +36,28 @@ class HighArray():
       return value
 
    # display
-   def display( self ):
-      print( *a )        # NOTE: This is a good way to display lists
-      return
+   def display(self):
+      print(*a)     # found syntax on https://www.geeksforgeeks.org/print-lists-in-python-4-different-ways/
+      return 
 
    # get max goes here
-   def getMax( self ):
-      max = 0
-      if len(a) == 0:
-            return -1
-      for i in range( len( a ) ):
-         if a[i] > max:
-            max = a[i]
-      return max
+   def getMax(self):
+      if a == []:
+        return -1
+      else:
+        return max(a)
    # noDupes goes here
-   def noDupes( self ):
-      global a
-      a = [*set(a)]
-      return a
-   
-# a little test to make sure the interpreter won't barf
+   def noDupes(self):
+    # define a, where the list is stored, as a global variable so that nuDupes can access and modify it.
+    global a   
+    a_noDupes = [] 
+    [a_noDupes.append(element) for element in a if element not in a_noDupes]
+    a = a_noDupes
+    # references:
+    # https://stackoverflow.com/questions/74412503/cannot-access-local-variable-a-where-it-is-not-associated-with-a-value-but
+    # https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
+    return a
+
+# a little test to make sure the interpreter won't barf [res.append(x) for x in test_list if x not in res]
 h1 = HighArray( 10 )
 h1.display()
--- homework/homework2/HighArrayTest.py
+++ homework/homework2/HighArrayTest.py
@@ -14,7 +14,6 @@ print( "   Initializing the list" )
 arr = HighArray( 100 )
 print( arr )
 
-print( "Lets check that the list is empty \n", arr.getMax() )
 print( "   Adding ten items" )
 arr.insert( 77 )                 # insert 10 items
 arr.insert( 99 )
@@ -29,7 +28,9 @@ arr.insert( 33 )
 
 print( "\n   Current list contents:" )
 arr.display()
-print( "max:", arr.getMax() )
+# TODO: put in a call to 'arr.getMax()'
+print("\nThe highest value in the list is: ")
+print(arr.getMax())
 
 print( "\n   Searching for 37:" )
 found = arr.find( 37 )
@@ -48,28 +49,21 @@ else:
 print( "\n   Adding and removing to test getMax function..." )
 arr.delete( 00 )          # delete 3 items
 arr.delete( 55 )
-arr.delete( 99 )
+
+max_numb = arr.getMax() #finding the maximum value in the list and storing it in max_numb.
+arr.delete( max_numb )  # deleting the highest value.
+
 arr.display()             # display items again
-# TODO: put in a call to 'arr.getMax()'
-print( "max:", arr.getMax() )
-arr.insert( 111 )  
-print( "\n   Lets test it again..." )
-arr.display()
-print( "max:", arr.getMax() )
-arr.insert( 22 )  
-print( "\n   One last time..." )
-arr.display()
-print( "max:", arr.getMax() )
-arr.insert( 99 )
+print("\n the number {} was removed from the list, now the largest number in the list is: {} ".format(max_numb, arr.getMax()))
+
 print( "\n   Adding 11, 00, 66, and 33 to make duplicates..." )
 arr.insert( 11 )
 arr.insert( 00 )
 arr.insert( 66 )
 arr.insert( 33 )
 arr.display()
-print( "\n   Removing dupes")
-arr.a = arr.noDupes()
-arr.getMax()
+print( "\n   Removing dupes ")
+arr.noDupes()
 arr.display()
 print( "\n   Adding five copies of 33..." )
 arr.insert( 33 )
@@ -79,6 +73,18 @@ arr.insert( 33 )
 arr.insert( 33 )
 arr.display()
 print( "\n   Removing dupes")
-arr.a = arr.noDupes()
+arr.noDupes()
 arr.display()
+print("\nThe highest value is: ", arr.getMax())
+print('Final Test:\nremove all elements from the list and check if arr.getMax() returns -1')
+arr.delete( 77 )
+arr.delete( 44 )
+arr.delete( 22 )
+arr.delete( 88 )
+arr.delete( 11 )
+arr.delete( 66 )
+arr.delete( 33 )
+arr.delete( 0 )
+print("\nREMOVED ALL ELEMENTS FROM THE LIST.\nHIGHEST VALUE IS: ", arr.getMax())
+
 print( "\n   TESTING COMPLETE!  THANKS FOR DROPPING BY..." )
