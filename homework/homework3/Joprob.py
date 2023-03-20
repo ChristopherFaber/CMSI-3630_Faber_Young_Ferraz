b61d721572ec3a603dbb02976f9b9f05f8a216f0
AYoung65 <122577974+AYoung65@users.noreply.github.com>
2023-03-20 10:09:59 -0700

    Update Joprob.py

--- homework/homework3/Joprob.py
+++ homework/homework3/Joprob.py
@@ -3,6 +3,8 @@ def joprob(n, c):
         return 1
     else:
         return (joprob(n-1,c)+c-1)%n+1
+#I got the equation from
+#https://cs.stackexchange.com/q/7048
 def main():
    n = int(input("Enter the size of your troop: "))
    c = int(input("Enter the number you wish to count by: "))
