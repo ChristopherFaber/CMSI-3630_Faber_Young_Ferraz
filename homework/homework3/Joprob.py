10a508c4d51642bf03b7de9070e4673f3bbe7b22
AYoung65 <122577974+AYoung65@users.noreply.github.com>
2023-03-19 19:59:16 -0700

    Add files via upload

---
+++ homework/homework3/Joprob.py
@@ -0,0 +1,13 @@
+def joprob(n, c):
+    if (n==1):
+        return 1
+    else:
+        return (joprob(n-1,c)+c-1)%n+1
+def main():
+   n = int(input("Enter the size of your troop: "))
+   c = int(input("Enter the number you wish to count by: "))
+   safe = joprob(n,c)
+   print("The safe spot for Jo is: ",safe)
+
+main()
+
